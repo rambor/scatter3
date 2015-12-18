892d171d806f672528c855be247c95aabcbd1c51
rambor <robert_p_rambo@hotmail.com>
2015-12-18 03:51:23 +0000

    labelled radiobuttons on files

---
+++ src/Scatter.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+
+/**
+ * Created by robertrambo on 17/12/2015.
+ */
+public class Scatter {
+    private JTabbedPane mainPane;
+    private JPanel panel1;
+    private JPanel filesPanel;
+    private JPanel analysisPanel;
+    private JPanel statusPanel;
+    private JProgressBar progressBar1;
+    private JPanel buttonPanel;
+    private JPanel chartsPanel;
+    private JButton removeButton;
+    private JTextField collectionNote;
+    private JPanel collectionFileList;
+    private JPanel dataFilesPanel;
+    private JList dataFilesList;
+    private JPanel loadPanel1;
+    private JRadioButton radioButtonLoad1;
+    private JButton clearButton1;
+    private JPanel loadPanel2;
+    private JPanel loadPanel3;
+    private JPanel loadPanel4;
+    private JButton clearButton2;
+    private JButton clearButton4;
+    private JButton clearButton3;
+    private JCheckBox convertNm1ToCheckBox;
+    private JRadioButton radioButtonLoad2;
+    private JRadioButton radioButtonLoad3;
+    private JRadioButton radioButtonLoad4;
+
+    private String version = "3.0";
+
+    public Scatter() { // constructor
+
+        mainPane.setTabLayoutPolicy(JTabbedPane.WRAP_TAB_LAYOUT);
+
+
+
+    }
+}
