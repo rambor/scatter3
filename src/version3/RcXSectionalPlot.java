a1fc7719cd4b1ef6f887760eca0ca773b9113122
rambor <robert_p_rambo@hotmail.com>
2016-01-14 21:03:14 +0000

    Added Buffer and Notes editor classes for updating Dataset information

---
+++ src/version3/RcXSectionalPlot.java
@@ -0,0 +1,13 @@
+package version3;
+
+import javax.swing.*;
+
+/**
+ * Created by robertrambo on 14/01/2016.
+ */
+public class RcXSectionalPlot {
+    private JSpinner spinnerRcH;
+    private JSpinner spinnerRcL;
+    private JLabel manualFile;
+    private JLabel manualRcLimits;
+}
